0a313b51147a07c4361c2e86772b979fdbc724a2
Nishan Wijethunga <nishan@the-ai.team>
2022-04-15 06:06:41 +0530

    fix(line-breaking): Update for safe URL check

--- packages/textkit/src/utils/isUrl.js
+++ packages/textkit/src/utils/isUrl.js
@@ -1,13 +1,38 @@
 /**
+ * reference https://github.com/segmentio/is-url/blob/master/index.js
+ */
+/* eslint-disable no-useless-escape */
+const protocolAndDomainRE = /^(?:\w+:)?\/\/(\S+)$/;
+const localhostDomainRE = /^localhost[\:?\d]*(?:[^\:?\d]\S*)?$/
+const nonLocalhostDomainRE = /^[^\s\.]+\.\S{2,}$/;
+
+/**
  * Checks if string is a URL
  *
  * @param  {string}  value value
  * @return {Boolean} is URL
  */
 const isUrl = value => {
-  // eslint-disable-next-line no-useless-escape
-  const regx = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-  return regx.test(value);
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  const match = value.match(protocolAndDomainRE);
+  if (!match) {
+    return false;
+  }
+
+  const everythingAfterProtocol = match[1];
+  if (!everythingAfterProtocol) {
+    return false;
+  }
+
+  if (localhostDomainRE.test(everythingAfterProtocol) ||
+      nonLocalhostDomainRE.test(everythingAfterProtocol)) {
+    return true;
+  }
+
+  return false;
 };
 
 export default isUrl;
