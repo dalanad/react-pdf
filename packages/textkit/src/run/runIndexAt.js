90786e30d14c1e784316c911c7ac41d72030b1bd
Pasindu <pasindu@the-ai.team>
2022-06-08 10:10:38 +0530

    feat(textkit): optimize runIndexAt function
    This commit improves the running time complexity of the runIndexAt function.
    Original: O(N)
    New: O(log N)

--- packages/textkit/src/run/runIndexAt.js
+++ packages/textkit/src/run/runIndexAt.js
@@ -1,7 +1,5 @@
 import * as R from 'ramda';
 
-import isBetween from '../utils/isBetween';
-
 /**
  * Get run index that contains passed index
  *
@@ -9,7 +7,16 @@ import isBetween from '../utils/isBetween';
  * @param  {Array}  runs array
  * @return {Array} run index
  */
-const runIndexAt = (n, runs) =>
-  R.findIndex(isBetween(R.prop('start'), R.prop('end'), n))(runs);
+const runIndexAt = (x, runs) => {
+  // binary search to find the index
+  const n = runs.length;
+  let k = -1;
+
+  const ok = (v) => v >= n || x < runs[v].end;
+  for(let b=n; b>=1; b=~~(b/2)){ // ~~ is used to get the integer output (instead of using Math.floor)
+    while(!ok(k+b)) k += b;
+  }
+  return k + 1;
+};
 
 export default R.curryN(2, runIndexAt);
