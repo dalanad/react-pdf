a61cf57427a90a5c1f446e2dcebc772b1cbbc713
Pasindu Wijesena <pasindu@the-ai.team>
2022-06-08 17:06:02 +0530

    fix(textkit): replace bitwise operation with standard function

--- packages/textkit/src/run/runIndexAt.js
+++ packages/textkit/src/run/runIndexAt.js
@@ -13,8 +13,8 @@ const runIndexAt = (x, runs) => {
   let k = -1;
 
   const ok = (v) => v >= n || x < runs[v].end;
-  for(let b=n; b>=1; b=~~(b/2)){ // ~~ is used to get the integer output (instead of using Math.floor)
-    while(!ok(k+b)) k += b;
+  for (let b = n; b >= 1; b = Math.floor(b / 2)) {
+    while (!ok(k + b)) k += b;
   }
   return k + 1;
 };
