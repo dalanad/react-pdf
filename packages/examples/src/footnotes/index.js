0edecee88caab2c2acb49d0f1dd55f2440224720
dalanad <dalana199981@gmail.com>
2022-09-20 23:30:24 +0530

    feat(footnotes): cutoff footnotes per line

--- packages/examples/src/footnotes/index.js
+++ packages/examples/src/footnotes/index.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   },
   body: {
     paddingTop: 35,
-    paddingBottom: 35,
+    paddingBottom: 65,
     paddingHorizontal: 35,
   },
   title: {
@@ -31,12 +31,28 @@ const styles = StyleSheet.create({
     margin: 12,
     fontFamily: 'Oswald',
   },
-  chapter: {
-    margin: 12,
-  },
   text: {
     fontSize: 14,
+    textAlign: 'justify',
     fontFamily: 'Times-Roman',
+  },
+  image: {
+    marginVertical: 15,
+    marginHorizontal: 100,
+  },
+  header: {
+    fontSize: 12,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'grey',
+  },
+  pageNumber: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
   },
 });
 let footnoteNumber = 0;
@@ -56,10 +72,13 @@ const Footnote = () => {
           <View>
             <Text style={{ fontSize: 10 }}>{x} . </Text>
           </View>
-          <View style={{ fontSize: 10 }}>
+          <View style={{ fontSize: 10, width: 'calc(100% - 30)' }}>
             <Text style={{ fontSize: 10 }} hyphenationCallback={e => [e]}>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit dolor sit
               amet, consectetur adipisicing elit consectetur adipisicing elit
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit dolor sit
+              amet, consectetur adipisicing elit consectetur adipisicing elit
+              line
             </Text>
           </View>
         </View>
@@ -72,18 +91,89 @@ const Footnote = () => {
 };
 export default () => (
   <Document>
-    <Page size="A5" debug={false}>
-      <View style={{ ...styles.text, ...styles.body }}>
+    <Page size="A5" debug={false} style={{ ...styles.body }}>
+      <View style={{ ...styles.text }}>
         <View style={{ position: 'relative' }}>
-          <Text>
+          <Text orphans={0} widows={0} debug>
+            Hello Footnotes <Footnote />
+          </Text>
+          <View
+            fixed
+            debug
+            style={{
+              position: 'absolute',
+              top: '100%',
+              width: '100%',
+            }}
+            renderFootnotes
+          />
+        </View>
+      </View>
+    </Page>
+    <Page size="A5" debug={false} style={{ ...styles.body }}>
+      <View style={{ ...styles.text }}>
+        <View style={{ position: 'relative' }}>
+          <Text orphans={0} widows={0} debug>
+            f footnotes in a single line asdasd asdasd Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit dolor sit amet, consectetur
+            adipisicing elit consectetur adipisicing elit Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit dolor sit amet, consectetur
+            adipisicing elit consectetur adipisicing elit line A
+            {[...Array(13)].map(() => (
+              <Footnote />
+            ))}
+            Large Number of footnotes in a single line
+            <Footnote /> Large Number of footnotes in a single line Large Number
+            of footnotes in a single line asdasd asdasd Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit dolor sit amet, consectetur
+            adipisicing elit consectetur adipisicing elit Lorem ipsum dolor sit
+            amet, consectetur adipisicing elit dolor sit amet, consectetur
+            adipisicing elit consectetur adipisicing elit linef footnotes in a
+            single line asdasd asdasd Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit dolor sit amet, consectetur adipisicing elit
+            consectetur adipisicing elit Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit dolor sit amet, consectetur adipisicing elit
+            consectetur adipisicing elit line
+          </Text>
+          <View
+            fixed
+            debug
+            style={{
+              position: 'absolute',
+              top: '100%',
+              width: '100%',
+            }}
+            renderFootnotes
+          />
+        </View>
+      </View>
+    </Page>
+    <Page size="A5" debug={false} style={{ ...styles.body }}>
+      <View style={{ ...styles.text }}>
+        <View style={{ position: 'relative' }}>
+          <Text debug>
             Large Number of footnotes in a single page&nbsp; & edge case
             containing reference shifting scenario. sit amet, consectetur
-            adipisicing elit. Quaerat adipisci
+            adipisicing elit. Quaerat adipisciLarge Number of footnotes in a
+            Large Number of footnotes in a single page&nbsp; & edge case
+            containing reference shifting scenario. sit amet, consectetur
+            adipisicing elit. Quaerat adipisciLarge Number of footnotes in a
+            single page&nbsp; & edge case containing reference shifting
+            scenario. sit amet, consectetur adipisicing elit. Quaerat adipisci
             <Footnote /> facere alias <Footnote />
+            <Footnote /> <Footnote /> <Footnote /> <Footnote /> <Footnote />
+            <Footnote />
+            <Footnote /> <Footnote /> <Footnote /> <Footnote /> <Footnote />
+            <Footnote />
             saepe aliquam
             <Footnote /> quia amet, <Footnote />
-            expedita aperiam dicta
-            <Footnote /> earum accusantium! <Footnote />
+            expedita aperiam dicta Large Number of footnotes in a single
+            page&nbsp; & edge case containing reference shifting scenario. sit
+            amet, consectetur adipisicing elit. Quaerat adipisciLarge Number of
+            footnotes in a single page&nbsp; & edge case containing reference
+            shifting scenario. sit amet, consectetur adipisicing elit. Quaerat
+            adipisci <Footnote /> earum accusantium! <Footnote /> <Footnote />{' '}
+            <Footnote /> <Footnote /> <Footnote />
             <Footnote /> Dicta sit facere hic <Footnote />
             itaque dolores
             <Footnote /> officia blanditiis
@@ -139,6 +229,7 @@ export default () => (
           </Text>
           <View
             fixed
+            debug
             style={{
               position: 'absolute',
               top: '100%',
@@ -148,9 +239,14 @@ export default () => (
           />
         </View>
       </View>
+      <Text
+        style={styles.pageNumber}
+        render={({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`}
+        fixed
+      />
     </Page>
-    <Page size="A5" debug={false}>
-      <View style={{ ...styles.text, ...styles.body }}>
+    <Page size="A5" debug={false} style={{ ...styles.body }}>
+      <View style={{ ...styles.text }}>
         <View style={{ position: 'relative' }}>
           <Text>
             Footnote&nbsp;
@@ -182,11 +278,8 @@ export default () => (
             facere alias saepe aliquam quia amet, expedita aperiam dicta earum
             accusantium! Dicta sit facere hic itaque dolores officia blanditiis
             vel?Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quaerat adipisci facere alias saepe aliquam quia amet, expedita
-            aperiam dicta earum accusantium! Dicta sit facere hic itaque dolores
-            officia blanditiis vel?Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Quaerat adipisci facere alias saepe aliquam quia
-            amet, expedita aperiam dicta earum accusantium! Dicta sit facere hic
+            <Footnote /> Quaerat adipisci facere alias saepe aliquam quia amet,
+            expedita aperiam dicta earum accusantium! Dicta sit facere hic
             itaque dolores officia blanditiis vel?Lorem ipsum dolor sit amet,
             consectetur adipisicing elit. Quaerat adipisci facere alias saepe
             aliquam quia amet, expedita aperiam dicta earum accusantium! Dicta
@@ -200,7 +293,10 @@ export default () => (
             officia blanditiis vel?Lorem ipsum dolor sit amet, consectetur
             adipisicing elit. Quaerat adipisci facere alias saepe aliquam quia
             amet, expedita aperiam dicta earum accusantium! Dicta sit facere hic
-            itaque dolores officia blanditiis vel?
+            itaque dolores officia blanditiis vel?Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Quaerat adipisci facere alias saepe
+            aliquam quia amet, expedita aperiam dicta earum accusantium! Dicta
+            sit facere hic itaque dolores officia blanditiis vel?
           </Text>
           <View
             fixed
@@ -214,8 +310,8 @@ export default () => (
         </View>
       </View>
     </Page>
-    <Page size="A5" debug={false} wrap>
-      <View style={{ ...styles.text, ...styles.body }}>
+    <Page size="A5" debug={false} wrap style={{ ...styles.body }}>
+      <View style={{ ...styles.text }}>
         <View style={{ position: 'relative' }}>
           <Text>Footnote outside Text : </Text>
           <Footnote />
